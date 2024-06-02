4e805c518555d9426fe9de962d340e38f4323525
Hank Barta <hbarta@gmail.com>
2024-06-02 17:57:11 -0500

    return tuple from process_line()

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-//use std::collections::HashMap;
+use std::collections::HashMap;
 use std::io;
 
 const TOKENS: &'static [&'static str] = &[
@@ -19,25 +19,32 @@ const TOKENS: &'static [&'static str] = &[
 //static mut MATCHED_VALS: HashMap<String, String> = HashMap::new();
 
 // process each line
-fn process_line(line: &String) {
+fn process_line(line: &String) -> (&str, &str) {
     for token in TOKENS {
         if line.contains(token) {
             let token_value = &line[token.len() + 1..line.len() - 1]; // extract value
-            println!("matched:{}, got \"{}\"", line, token_value);
+            //println!("matched:{}, got \"{}\"", line, token_value);
+            return (*token, token_value)
         }
     }
+    ("","")
 }
 
 fn main() -> io::Result<()> {
     let mut buffer = String::new();
+    let mut matched_vals: HashMap<String, String> = HashMap::new();
     loop {
         // read input a line at a time
         buffer.clear();
         let r = io::stdin().read_line(&mut buffer)?;
-        process_line(&buffer);
+        let (tag, val) = process_line(&buffer);
+        if tag.len() > 0 {
+            matched_vals.insert(tag.to_string(), val.to_string());
+        }
         if r == 0 {
             break;
         }
     }
+    println!("{:?}", matched_vals);
     Ok(())
 }
