e7c1a33f3e0626c38c20db2263efef15d925c2b8
Hank Barta <hbarta@gmail.com>
2024-06-02 12:38:26 -0500

    stub for process_line()

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use std::io;
 
+// process each line
+fn process_line(line: &String) {
+    println!("{}", line);
+}
+
 fn main() -> io::Result<()> {
     let mut buffer = String::new();
     loop {
+        // read input a line at a time
         buffer.clear();
         let r = io::stdin().read_line(&mut buffer)?;
-        println!("{}", buffer);
+        process_line(&buffer);
         if r == 0 {
             break;
         }
